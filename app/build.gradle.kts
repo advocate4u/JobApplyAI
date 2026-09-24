0eec0f46c057090302d754b6981d9731cbc1fd62
advocate4u <advocatemittal4u@gmail.com>
2026-09-24 16:41:03 +0530

    test: add unit test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.work:work-runtime-ktx:2.10.0")
+    testImplementation("junit:junit:4.13.2")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
